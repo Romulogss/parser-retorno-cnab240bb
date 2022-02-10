63cb48d9511749f65320e67d24cb5355279432aa
Bruno Roberto Vasconcelos Tonia <brunotonia@bb.com.br>
2022-02-10 13:55:13 -0300

    organização das pastas de documentação e modelo, adicção da classe de leitura do arquivo cnab

--- src/main/java/br/com/brvt/App.java
+++ src/main/java/br/com/brvt/App.java
@@ -1,21 +1,5 @@
 package br.com.brvt;
 
-import java.util.ArrayList;
-
-import br.com.brvt.Modelo.CompTransfEntreContasBB;
-import br.com.brvt.Modelo.HeaderArquivo;
-import br.com.brvt.Modelo.HeaderLoteAB;
-import br.com.brvt.Modelo.SegmentoA;
-import br.com.brvt.Modelo.SegmentoB;
-import br.com.brvt.Modelo.SegmentoZ;
-import br.com.brvt.Util.ArquivoAbrir;
-import br.com.brvt.Util.ArquivoGravar;
-import br.com.brvt.Util.GerarCompTransferencia;
-import br.com.brvt.Util.ParserHeaderArquivo;
-import br.com.brvt.Util.ParserHeaderLoteAB;
-import br.com.brvt.Util.ParserSegmentoA;
-import br.com.brvt.Util.ParserSegmentoB;
-import br.com.brvt.Util.ParserSegmentoZ;
 import br.com.brvt.Util.Util;
 
 public class App {
